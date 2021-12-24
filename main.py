d423dcea5d3191d84732514b35c33077f35f8d8e
tomita <319012kei@users.noreply.github.com>
2021-12-24 15:56:01 +0900

    Initial commit

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from flask import Flask, jsonify, abort
 import json
 
-main = Flask(__name__)
+app = Flask(__name__)
 
-@main.route("/hello")
+@app.route('/')
+def welcome():
+    
+
+@app.route("/hello")
 def hello():
     abort(404, 'hello abort')
     return jsonify({'message': 'error code 404'})
 
-@main.errorhandler(404)
+@app.errorhandler(404)
 def error_404(e):
     print('httpステータス:{}, メッセージ:{}, 詳細:{}'.format(e.code, e.name, e.description))
     return jsonify({'message': 'internal server error', 'action': 'call me'}), 404
 
-@main.route("/api/v1/hello")
+@app.route("/api/v1/hello")
 def hello():
     return jsonify({
         "message": "Hello World!"
